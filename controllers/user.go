94c2bc5dd4c0853b9ee36237e1c64d3e38635fdb
Jake Kalstad <JakeKalstad@Gmail.com>
2016-02-02 23:05:14 -0500

    Scaffolding

---
+++ controllers/user.go
@@ -0,0 +1,18 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+type UserController struct {
+}
+
+func (c *UserController) Save(ctx *gin.Context) {
+
+}
+
+func (c *UserController) Delete(ctx *gin.Context) {
+
+}
+
+func (c *UserController) Retrieve(ctx *gin.Context) {
+
+}
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Configuration struct {
+}
+
+type Server struct {
+	*gin.Engine
+	Config      Configuration
+	Controllers []Controller
+}
+
+func (s Server) Get(url string, action func(c Context)) {
+	s.GET(url, func(ctx *gin.Context) { action(Context{ctx}) })
+}
+
+func (s Server) Post(url string, action func(c Context)) {
+	s.POST(url, func(ctx *gin.Context) { action(Context{ctx}) })
+}
+
+func (s Server) Delete(url string, action func(c Context)) {
+	s.DELETE(url, func(ctx *gin.Context) { action(Context{ctx}) })
+}
+
+func (s Server) Put(url string, action func(c Context)) {
+	s.PUT(url, func(ctx *gin.Context) { action(Context{ctx}) })
+}
+
+func (c Configuration) readConfig() {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+}
+
+type Context struct {
+	*gin.Context
+}
+
+func ping(c Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func main() {
+	config := Configuration{}
+	config.readConfig()
+	server := Server{
+		gin.Default(),
+		config,
+	}
+
+	server.Get("/ping", ping)
+	server.RegisterControllers()
+	server.Run()
+}
